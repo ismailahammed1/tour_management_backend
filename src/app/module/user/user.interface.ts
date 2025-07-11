896248c90d743d4a0bb32d328bae9081fa78a689
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-07-12 00:40:54 +0600

    create user model

---
+++ src/app/module/user/user.interface.ts
@@ -0,0 +1,41 @@
+import { Types } from "mongoose";
+
+export enum Role{
+    SUPER_ADMIN="SUPER_ADMIN",
+    ADMIN="ADMIN",
+    USER="USER",
+    GUIDE="GUIDE",
+}
+
+//auth provider list 
+/**
+ * emaii,password
+ * google authentication
+ */
+export interface IAuthProviver{
+    provider:string;//goole ,credintial
+    providerID:string;
+}
+
+export enum isActive{
+    ACTIVE="ACTIVE",
+    INACTIVE="INACTIVE",
+    BLOCKED="BLOCKED",
+}
+
+export interface Iuser{
+    name:string;
+    email: string;
+    password?:string;
+    picture?:string;
+    phone?:string;
+    address?:string;
+    isDeleted?:string;
+    isActive?:isActive;
+    isVerified?:string;
+
+    auths:IAuthProviver;
+    role:Role;
+    bookings?:Types.ObjectId[];
+    guides?:Types.ObjectId[];
+}
