7a048c019e404ef1f16b3717dc2376907bcdc134
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-07-13 13:26:03 +0600

    create user ruter setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import  express, { Request, Response } from "express";
+import { UserRoutes } from "./app/module/user/user.route";
+import cors from "cors"
+
 
 const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+app.use("/api/v1/user",UserRoutes)
+
 app.get("/", (req:Request, res:Response)=>{
     res.status(200).json({
         massage:"helllo server "
