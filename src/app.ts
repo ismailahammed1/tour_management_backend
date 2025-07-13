f53c547b03b32bcfae2c273ad650d652b28976ab
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-07-13 13:59:00 +0600

    create user ruter setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import  express, { Request, Response } from "express";
-import { UserRoutes } from "./app/module/user/user.route";
 import cors from "cors"
+import { router } from "./app/routes";
 
 
 const app = express();
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 
-app.use("/api/v1/user",UserRoutes)
+app.use("/api/v1",router)
 
 app.get("/", (req:Request, res:Response)=>{
     res.status(200).json({
---
+++ src/app/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { UserRoutes } from "../module/user/user.route";
+
+
+export const router=Router();
+
+const moduleRouter=[{
+    path:"/user",
+    route:UserRoutes,
+}
+]
+moduleRouter.forEach((route)=>{
+    router.use(route.path, route.route )
+})
