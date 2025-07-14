eeb52f87f75de5eccd176b329a3e1655c5e145e1
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-07-14 13:57:52 +0600

    module 1 done catch async and sendresponse

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextFunction, Request, Response } from "express";
 import { userService } from "./user.services";
 import catchAsync from "../../utils/catchAsync";
