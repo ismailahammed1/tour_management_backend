ba5b8de248e8cc1207d03ddae3de9ef56b3f6dcc
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-07-16 01:30:02 +0600

    user validation paswrod add with credintial

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,9 +1,14 @@
-import { Router } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response, Router } from "express";
 import userController from "./user.controller";
+import z from "zod";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
-const router=Router()
+const router = Router();
 
-router.post("/register", userController.createUser)
-router.get("/all-users", userController.getAllUser)
 
-export const UserRoutes=router;
+router.post("/register", validateRequest(createUserZodSchema),userController.createUser);
+router.get("/all-users", userController.getAllUser);
+
+export const UserRoutes = router;
--- src/app/module/user/user.services.ts
+++ src/app/module/user/user.services.ts
@@ -1,11 +1,20 @@
-import { Iuser } from "./user.interface";
+import httpStatus from "http-status-codes";
+import appError from "../../errorHelpers/AppError";
+import { IAuthProviver, Iuser } from "./user.interface";
 import { User } from "./user.model";
 
 const createUser=async (payload:Partial<Iuser>)=>{
-        const {name,email}=payload;
+        const {email, ...rest}=payload;
+        const isUserExist = await User.findOne({email})
+        if (isUserExist) {
+            throw new appError(httpStatus.BAD_REQUEST,"user Already Exist");
+
+        }
+        const authProvider:IAuthProviver={provider:"credintial",providerID:email as string}
         const user=await User.create({
-            name,
-            email
+            email,
+            auths:authProvider,
+            ...rest
         })
         return user
 }
