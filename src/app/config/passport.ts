1a6b616e61d69028cadaa483c54a57ea2f49a61d
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-07-28 10:48:56 +0600

    eror setup

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -8,6 +8,47 @@ import {
 import { envVars } from "./env";
 import { User } from "../module/user/user.model";
 import { Role } from "../module/user/user.interface";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcryptjs from "bcryptjs";
+
+passport.use(
+    new LocalStrategy({
+        usernameField: "email",
+        passwordField: "password"
+    }, async (email: string, password: string, done) => {
+        try {
+            const isUserExist = await User.findOne({ email })
+        if (!isUserExist) {
+          return done(null, false, { message: "user dose not exist" });
+        }
+
+        const isGoogleAuthenticated = isUserExist.auths.some(
+          (providerObjects) => providerObjects.provider == "google"
+        );
+
+        if (isGoogleAuthenticated) {
+          return done(null, false, {
+            message:
+              "You have authenticated through Google. So if you want to login with credentials, then at first login with google and set a password for your Gmail and then you can login with email and password.",
+          });
+        }
+
+        const isPasswordMatched = await bcryptjs.compare(
+          password as string,
+          isUserExist.password as string
+        );
+
+        if (!isPasswordMatched) {
+          return done(null, false, { message: "password dose not match" });
+        }
+        return done(null, isUserExist);
+      } catch (error) {
+        console.log(error);
+        done(error);
+      }
+    }
+  )
+);
 
 passport.use(
   new GoogleStrategy(
--- src/app/middleware/golobalErrorHandler.ts
+++ src/app/middleware/golobalErrorHandler.ts
@@ -4,27 +4,78 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import appError from "../errorHelpers/AppError";
-// Error-handling middleware
+import { ZodError } from "zod";
+
+// Global Error Handler Middleware
 export const golobalErrorHandler = (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+
+     if (envVars.NODE_ENV ==="development") {
+      console.log(err);
+      
+    }
   let statusCode = 500;
   let message = "Something went wrong";
-  if (err instanceof appError) {
-    statusCode=err.statusCode
-    message=err.message
-  }else if(err instanceof Error){
-    statusCode=500;
-    message=err.message
+
+  // Duplicate key error (MongoDB unique constraint violation)
+  if (err.code === 11000) {
+    const matchedArray = err.message.match(/"([^"]*)"/);
+    statusCode = 400;
+    const duplicateField = matchedArray ? matchedArray[1] : "Field";
+    message = `${duplicateField} already exists!`;
+  }
+
+  // Invalid MongoDB ObjectId
+  else if (err.name === "CastError") {
+    statusCode = 400;
+    message = "Invalid MongoDB ObjectID. Please provide a valid id";
+  }
+
+  // Mongoose validation error
+  else if (err.name === "ValidationError") {
+    statusCode = 400;
+    const errors = Object.values(err.errors);
+      const errorSources: any[] = [];
+    errors.forEach((errorObj: any) => {
+      errorSources.push({
+        path: errorObj.path,
+        message: errorObj.message,
+      });
+    });
+    message = "Validation Error";
+  }
+
+  else if (err instanceof ZodError) {
+  statusCode = 400;
+  message = "Validation Error from Zod";
+  const errorSources: any[] = [];
+
+  err.issues.forEach((issue: any) => {
+    errorSources.push({
+      path: issue.path.join("."),
+      message: issue.message,
+    });
+  });
+}
+  // Custom AppError
+  else if (err instanceof appError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  }
+
+  // Generic Error
+  else if (err instanceof Error) {
+    message = err.message;
   }
 
   res.status(statusCode).json({
     success: false,
     message,
-    err,
-    stack: envVars.NODE_ENV === "development" ? err.stack : null,
+    err: envVars.NODE_ENV === "development" ? err : undefined,
+    stack: envVars.NODE_ENV === "development" ? err.stack : undefined,
   });
 };
